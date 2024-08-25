3db0bac40203f20a0df82b3f7fd9a8fa761ba882
livia-xavier2008 <xavierlivi4@gmail.com>
2024-08-25 15:49:08 -0300

    adicionei objetos
    a

---
+++ src/Produtos/produtos.jsx
@@ -0,0 +1,125 @@
+export default function Oferta(){ 
+
+    const [listaObjetos, setListaObjetos] = useState([
+        {
+            id: 1,
+            nome: "vestido feminino inspiração farm azul",
+            image: "https://www.ditorini.com.br/cdn/shop/files/Sb08d9342141a4adebead395c4d8a4084Q_864x.webp?v=1701134675",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 200
+        },
+        {
+            id: 2,
+            nome: "vestido feminino preto estampado",
+            image: "https://static.ecosweb.com.br/public/produtos/moda-feminina/vestido-longo/vestido-onca-abstrata-em-malha-de-viscose_373151_301_1.webp",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 120
+        },
+        {
+            id: 3,
+            nome: "vestido feminino vermelho brilhante curto",
+            image: "https://http2.mlstatic.com/D_NQ_NP_989467-MLB72576339027_102023-O.webp",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 170
+        },
+        {
+            id: 4,
+            nome: "vestido feminino vermelho com fenda",
+            image: "https://i.pinimg.com/736x/1a/39/65/1a3965b3fdcd4907a0b5425996f6ae78.jpg",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 380
+        },
+        {
+            id: 5,
+            nome: "vestido femininopreto tubinho com decote",
+            image: "https://modamix.vteximg.com.br/arquivos/ids/182726-0-0/modamix_feminino_6_junho_49.png?v=637906428278430000",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 230
+        },
+        {
+            id: 6,
+            nome: "vestido feminino tipo camisa estampado",
+            image: "https://gamborini.com/cdn/shop/products/vestido-longo-feminino-casual-santorini-0-loja-895035_800x.jpg?v=1715251108",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 150
+        },
+        {
+            id: 7,
+            nome: "vestido feminino verde estampado",
+            image: "https://img.irroba.com.br/fit-in/1000x1600/filters:fill(fff):quality(80)/villaven/catalog/4513/4-img-5029.jpg",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 110
+        },
+        {
+            id: 8,
+            nome: "vestido feminino rosa estampado",
+            image: "https://www.belaprime.com/cdn/shop/files/Sd8bb99eefdb94eef91e1256ddb1a1dd9k.webp?v=1707663986",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 130
+        },
+        {
+            id: 9,
+            nome: "vestido feminino de cetim azul",
+            image: "https://ae01.alicdn.com/kf/S20ec585447384198baa63f365482f2aaE/Festa-2023-dividir-verde-sexy-vestido-de-cetim-para-as-mulheres-preto-senhora-foraml-clube-maxi.jpg",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 190
+        },
+        {
+            id: 10,
+            nome: "vestido feminino de cetim verde",
+            image: "https://photos.enjoei.com.br/vestido-cetim-verde-costas-nua-83601261/800x800/czM6Ly9waG90b3MuZW5qb2VpLmNvbS5ici9wcm9kdWN0cy81MzgwODY5LzA1NGUxNGIwNGFkZjVhNGQzNzE1NDg2NjIwMmVlYjUxLmpwZw",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 190
+        },
+        {
+            id: 11,
+            nome: "vestido feminino azul royal",
+            image: "https://cdn.awsli.com.br/600x700/2233/2233931/produto/243430635/design-sem-nome---2023-11-27t170856-671-eaf7g2dprw.jpg",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 100
+        },
+        {
+            id: 12,
+            nome: "vestido feminino princesa branco",
+            image: "https://http2.mlstatic.com/D_NQ_NP_966910-MLB72164647189_102023-O.webp",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 125
+        },
+        {
+            id: 13,
+            nome: "vestido feminino plus size preto",
+            image: "https://down-br.img.susercontent.com/file/bf6cebc9a56af2af265971d4e27acd1d",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 150
+        },
+        {
+            id: 14,
+            nome: "vestido feminino pink brilhante",
+            image: "https://cdn.awsli.com.br/800x800/2032/2032729/produto/203743984/whatsapp-image-2023-02-16-at-09-57-30-ilnkyz.jpg",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 250
+        },
+        {
+            id: 15,
+            nome: "vestido feminino verde",
+            image: "https://acdn.mitiendanube.com/stores/003/275/653/products/fotos_arrumadadas-dropi-b5218d298f2b93b60716968632902904-1024-1024.jpg",
+            tipo: 'feminino',
+            categoria: ['feminino, vestido'],
+            preco: 80
+        },
+
+    ])}
