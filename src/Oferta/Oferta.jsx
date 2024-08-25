d74297806dddaf648c2d607c21c9f6277c25306d
livia-xavier2008 <xavierlivi4@gmail.com>
2024-08-25 15:44:37 -0300

    adiocionei os produtos
    todos

---
+++ src/Oferta/Oferta.jsx
@@ -0,0 +1,87 @@
+export default function Oferta(){ 
+
+        const [listaObjetos, setListaObjetos] = useState([
+            {
+                id: 1,
+                nome: "vestido feminino azul royal",
+                image: "https://cdn.awsli.com.br/600x700/2233/2233931/produto/243430635/design-sem-nome---2023-11-27t170856-671-eaf7g2dprw.jpg",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 100
+            },
+            {
+                id: 2,
+                nome: "vestido feminino princesa branco",
+                image: "https://http2.mlstatic.com/D_NQ_NP_966910-MLB72164647189_102023-O.webp",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 125
+            },
+            {
+                id: 3,
+                nome: "vestido feminino plus size preto",
+                image: "https://down-br.img.susercontent.com/file/bf6cebc9a56af2af265971d4e27acd1d",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 150
+            },
+            {
+                id: 4,
+                nome: "vestido feminino pink brilhante",
+                image: "https://cdn.awsli.com.br/800x800/2032/2032729/produto/203743984/whatsapp-image-2023-02-16-at-09-57-30-ilnkyz.jpg",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 250
+            },
+            {
+                id: 5,
+                nome: "vestido feminino verde ",
+                image: "https://acdn.mitiendanube.com/stores/003/275/653/products/fotos_arrumadadas-dropi-b5218d298f2b93b60716968632902904-1024-1024.jpg",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 80
+            },
+            {
+                id: 6,
+                nome: "vestido feminino ",
+                image: "",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 
+            },
+            {
+                id: 7,
+                nome: "vestido feminino ",
+                image: "",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 
+            },
+            {
+                id: 8,
+                nome: "vestido feminino ",
+                image: "",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 
+            },
+            {
+                id: 9,
+                nome: "vestido feminino ",
+                image: "",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 
+            },
+            {
+                id: 10,
+                nome: "vestido feminino ",
+                image: "",
+                tipo: 'feminino',
+                categoria: ['feminino, vestido'],
+                preco: 
+            },
+    
+    
+        ])}
+    
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Home/Home'
+import Oferta from './Oferta'
+import Produtos from './Produtos'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route parh='/' element={<Home/>}/>
+        <Route parh='/Oferta' element={<Oferta/>}/>
+        <Route parch = '/Produtos' element={<Produtos/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
-
-
 
